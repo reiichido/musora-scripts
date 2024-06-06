d6535e6cfbb63a26730b03a58f5e4931c3abeb6c
reiichido <rei.ichido1@gmail.com>
2024-06-06 15:46:16 +0200

    Create violentmonkey-soundlice-configuration.js

---
+++ violentmonkey-soundlice-configuration.js
@@ -0,0 +1,342 @@
+// ==UserScript==
+// @name        SoundSlice automation
+// @namespace   Violentmonkey Scripts
+// @match       *://www.soundslice.com/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 4/10/2024, 9:46:19 AM
+// ==/UserScript==
+
+var configJson = {
+  doWaitMessage: true,
+  hideVideo: true,
+  metronome: {
+    enable: false,
+    volume: 20,
+    // 0- Only downbeats
+    // 1- Every beat
+    option:0,
+    countIn: {
+      enable: false,
+      // 1- Before plaback
+      // 2- During loops
+      // 3- Always
+      option: 2,
+      // 1-3
+      barCount: 3
+    },
+  },
+  speed: 100,
+  video : {
+    useSynth: false,
+    // useSynth false AND drumless available
+    useDrumless: false
+  },
+  useLoop: false,
+	settings: {
+    masterVolume: 50,
+		// -21 -> 60, default 0
+		zoomLevel: 0,
+		layout: {
+			// scrollable / paged, default scrollable
+			mode: "scrollable",
+			scrollable: {
+				// default false
+				horizontalNotation: false,
+				// if true, default false
+				proportionalNotation: false,
+				// if proportionalNotation false
+				// min 25, max: screen dependant
+				staveWidth: {
+					doModify: false,
+					doFitToScreen: false,
+					staveWidthValue: 25
+				},
+				// if proportionalNotation true
+				// 20-80, default 50
+				value: 50
+
+			},
+			paged: {
+				// if proportionalNotation false
+				// min 25, max: screen dependant
+				staveWidth: {
+					doModify: false,
+					doFitToScreen: false,
+					value: 25
+				},
+				// if proportionalNotation true
+				// auto/1/2, deafult auto
+				pagePerSecond:"auto"
+			}
+		},
+		transposition: {
+			// -12 -> 12, default 0
+			note: 0,
+			// Synth only, default true
+			transposeAudioPlayback: true
+		},
+		playHeadStyle: {
+			// 1-4, default 1
+			shape: 1,
+			// 1-4, default 1
+			color: 1,
+			// default true
+			highlightNotes: true,
+			// deafult false
+			highlightBar: false
+		}
+	},
+  timeout: 300,
+	temp: 1
+};
+
+
+if(configJson.doWaitMessage) {
+  window.addEventListener('message', event => {
+    if(event.data.custom) {
+      var caller = event.data.caller;
+      // TODO override config for specific caller
+      switch(event.data.caller.split("/")[4]) {
+        case "songs":
+          //configJson.hideVideo=false;
+          configJson.settings.masterVolume=70;
+          configJson.settings.zoomLevel=10;
+          configJson.metronome.enable=true;
+          configJson.metronome.volume=40;
+          configJson.metronome.countIn.enable=true;
+          break;
+        case "method":
+          configJson.settings.masterVolume=85;
+          configJson.metronome.enable=true;
+          configJson.metronome.volume=40;
+          configJson.metronome.countIn.enable=true;
+          break;
+        case "workouts":
+          break;
+        case "packs":
+          configJson.hideVideo=true;
+          break;
+        case "courses":
+          break;
+        case "quick-tips":
+          break;
+        case "rudiments":
+          break;
+      }
+      processConfiguration();
+    }
+  });
+} else {
+  processConfiguration();
+}
+
+function processConfiguration() {
+  setTimeout(() => {
+    hideVideo(configJson.hideVideo);
+    //setMasterVolume(configJson.masterVolume);
+    setMetronome(configJson.metronome);
+    setLoop(configJson.useLoop);
+    setSpeed(configJson.speed);
+    setVideo(configJson.video);
+    setSettings(configJson.settings);
+
+    // Does not work in FF
+    //setFullScreen();
+  }, configJson.timeout);
+}
+
+function hideVideo(option) {
+  if(option) {
+    document.getElementById("video-sidebar").style.cssText = "visibility:collapse";
+  }
+}
+
+function setMetronome(options) {
+  handleCheckBoxChange("toggle-metronome", options.enable);
+  if(options.enable) {
+    handleInputValueChange("metronomevol", options.volume);
+    handleInputValueChange("metronome-every-beat", options.option);
+  }
+  var countinOptions = options.countIn;
+  handleCheckBoxChange("countin-toggle", countinOptions.enable);
+  if(countinOptions.enable) {
+      handleInputValueChange("metronome-loop-countin", countinOptions.option);
+      handleInputValueChange("countinlen", countinOptions.barCount);
+  }
+}
+
+function setSpeed(speed, doObserve = true) {
+  document.getElementsByClassName("speedvalue")[0].dispatchEvent(getMouseDownEvent());
+  document.getElementsByClassName("speedinp")[0].value = speed;
+  document.getElementsByClassName("artist-info")[0].dispatchEvent(getMouseDownEvent());
+  if(doObserve) {
+    var observer = new MutationObserver(function (mutations) {
+      if(mutations.length === 1) {
+        setSpeed(speed, false);
+        observer.disconnect();
+      }
+    });
+
+    let options = {
+      childList: true,
+      subtree: true
+    };
+
+    observer.observe(document.getElementsByClassName("speedvaluetext")[0], options);
+  }
+}
+
+function setVideo(options) {
+  if(options.useSynth) {
+    document.getElementById("usesynth").click();
+  } else {
+    document.getElementById("midi-toggle").dispatchEvent(getMouseDownEvent());
+    var isDrumlessActive = isButtonActive(document.getElementById("realrectoggle").querySelector("#synth-toggle-tooltip"));
+    if(isDrumlessActive) {
+      var drumlessOpt = options.useDrumless ? 1 : 0
+      document.getElementById("realrectoggle").querySelectorAll("label.visible")[drumlessOpt].click()
+    }
+  }
+}
+
+function setLoop(ok = true) {
+  console.log(ok);
+  var loopButton = document.getElementsByClassName("loopbutton")[0];
+  console.log(loopButton);
+  if((ok && !isButtonActive(loopButton)) || (!ok && isButtonActive(loopButton))) {
+    loopButton.dispatchEvent(getPointerDownEvent());
+  }
+}
+
+function setFullScreen(ok = true) {
+  var pointerDownEvent = new PointerEvent("pointerdown", {bubbles: true, cancellable: false});
+  var fullscreenButton = document.getElementById("fullscreenbutton");
+  if((ok && !isButtonActive(fullscreenButton)) || (!ok && isButtonActive(fullscreenButton))) {
+    fullscreenButton.click();
+    fullscreenButton.dispatchEvent(getMouseDownEvent());
+  }
+}
+
+function setSettings(config) {
+  var settingsButton = document.getElementById("toggle-settings");
+  settingsButton.dispatchEvent(getPointerDownEvent());
+
+  setTimeout(() => {
+    handleInputValueChange("mainvol", config.masterVolume);
+    handleInputValueChange("resizerange", config.zoomLevel);
+
+    setLayout(config.layout);
+    setSettingsTransposition(config.transposition);
+    setSettingsPlayHeadStyle(config.playHeadStyle);
+
+    settingsButton.dispatchEvent(getPointerDownEvent());
+
+  }, 300);
+}
+
+function setLayout(options) {
+  if(options.mode === "scrollable") {
+    setLayoutScrollable(options.scrollable);
+  } else if(options.mode === "paged") {
+    setLayoutPaged(options.paged);
+  }
+}
+
+function setLayoutScrollable(options) {
+  var scrollableCheckBox = document.getElementById("vtform").getElementsByTagName("input")[0];
+  if(!scrollableCheckBox.checked) {
+    scrollableCheckBox.click();
+  }
+
+  var horizontalNotationButton = document.getElementById("horiztoggle");
+  if(options.horizontalNotation && !isButtonActive(horizontalNotationButton) || !options.horizontalNotation && isButtonActive(horizontalNotationButton)) {
+      horizontalNotationButton.dispatchEvent(getPointerDownEvent());
+  }
+
+  if(!options.horizontalNotation) {
+    setLayoutStaveWidth(options.staveWidth);
+  } else {
+    handleCheckBoxChange("proportionalckbox", options.proportionalNotation);
+    if(options.proportionalNotation) {
+      handleInputValueChange("proportionalslider", options.value);
+    }
+  }
+}
+
+function setLayoutPaged(options) {
+  var pageCheckBox = document.getElementById("vtform").getElementsByTagName("input")[1];
+    if(!pageCheckBox.checked) {
+      pageCheckBox.click();
+    }
+
+  setLayoutStaveWidth(options.staveWidth);
+
+  var ppsOpt = 0;
+  switch(options.pagePerSecond) {
+      case "auto":
+        ppsOpt = 0;
+        break;
+      case "1":
+        ppsOpt = 1;
+        break;
+      case "2":
+        ppsOpt = 2;
+        break;
+  }
+  var ppsOptChk = document.getElementById("numpagesui").getElementsByTagName("input")[ppsOpt];
+  ppsOptChk.dispatchEvent(getChangeEvent());
+}
+
+function setLayoutStaveWidth(options) {
+  if(options.doModify) {
+    var staveWidthElement = document.getElementById("stavewidthrg");
+    var value = options.value;
+    if(options.doFitToScreen) {
+      value = staveWidthElement.max
+    }
+    handleInputValueChange("stavewidthrg", value)
+  }
+}
+function setSettingsTransposition(options) {
+  handleInputValueChange("transposeslider", options.note)
+  if(document.getElementById("usesynth").classList.contains("toggle-active")) {
+    handleInputValueChange("transposeaudio", options.transposeAudioPlayback)
+  }
+}
+
+function setSettingsPlayHeadStyle(options) {
+  handleInputValueChange("playhead-shape", options.shape);
+  handleInputValueChange("playhead-color", options.color);
+  handleCheckBoxChange("highlight-notes", options.highlightNotes);
+  handleCheckBoxChange("highlight-bar-option", options.highlightBar);
+}
+
+function isButtonActive(button) {
+  return button.classList.contains("active");
+}
+
+function handleInputValueChange(elementId, value) {
+  var elt = document.getElementById(elementId);
+  elt.value = value;
+  elt.dispatchEvent(getChangeEvent());
+}
+
+function handleCheckBoxChange(elementId, value) {
+  var elt = document.getElementById(elementId);
+  elt.checked = value;
+  elt.dispatchEvent(getChangeEvent());
+}
+
+function getChangeEvent() {
+  return new Event("change", {bubbles: true, cancellable: false});
+}
+
+function getPointerDownEvent() {
+  return new PointerEvent("pointerdown", {bubbles: true, cancellable: false});
+}
+
+function getMouseDownEvent() {
+  return new MouseEvent("mousedown", {bubbles: true, cancellable: false});
+}
