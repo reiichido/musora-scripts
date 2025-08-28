0697ee9128d4f7a4f6c9a2fcdfab91f3ee2a7f18
reiichido <rei.ichido1@gmail.com>
2025-08-28 18:08:59 +0200

    Update violentmonkey-musora-to-soundslice.js

--- violentmonkey-musora-to-soundslice.js
+++ violentmonkey-musora-to-soundslice.js
@@ -2,21 +2,21 @@
 // @name        Musora send SoundSlice iframe message
 // @namespace   Violentmonkey Scripts
 // @match       https://www.musora.com/
-// @include     /\.musora\.com\/.*\/(songs|method|workouts|packs|courses|quick-tips|rudiments).*/
+// @include     /\.musora\.com\/.*\/(songs|method|workouts|packs|courses|quick-tips|rudiments|song-tutorials).*/
 // @grant       none
-// @version     1.0
-// @author      rei.ichido
+// @version     1.1
+// @author      -
 // @description 4/13/2024, 12:17:03 PM
 // ==/UserScript==
 
-var app = document.getElementById("app");
+var app = document.querySelector("#app");
 if(app) {
-  var observer = new MutationObserver(function (mutations) {
-    var ssIFrame = document.getElementById("ssEmbed");
+  var observerSoundSlice = new MutationObserver(function (mutations) {
+    var ssIFrame = document.querySelector("#ssEmbed");
     if(ssIFrame) {
-      document.getElementById("ssEmbed").onload = function() {
+      ssIFrame.onload = function() {
         ssIFrame.contentWindow.postMessage({custom: true, caller: window.location.href}, ssIFrame.src);
-        observer.disconnect();
+        observerSoundSlice.disconnect();
       }
     }
   });
@@ -26,5 +26,27 @@ if(app) {
     subtree: true
   };
 
-  observer.observe(document.getElementById("app"), options);
+  observerSoundSlice.observe(app, options);
 }
+
+// Set Vimeo volume
+if(app) {
+  var observerVimeo = new MutationObserver(function (mutations) {
+    var volInput = document.querySelector("input.volume-range");
+    if(volInput) {
+      setTimeout(() => {
+        volInput.value = 70;
+        volInput.dispatchEvent(new Event("input", {bubbles: true, cancellable: false}));
+        observerVimeo.disconnect();
+      }, 1000);
+    }
+  });
+
+  let options = {
+    childList: true,
+    subtree: true
+  };
+
+  observerVimeo.observe(app, options);
+}
+
